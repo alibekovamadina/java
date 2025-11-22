5a249f7be19aa53cec218948dc84f528111b6178
itschool <itschool@tou.edu.kz>
2025-11-22 12:00:31 +0500

    начало2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // выводит окошко на экран
 // alert('madina')
 
-document.querySelector('info__link').addEventListener(\click , function () {
+document.querySelector('.info__link').addEventListener('click' , function () {
     this.remove();
-    document.querySelector('info__text').classList.add('info__text--active');
+    document.querySelector('.info__text').classList.add('info__text--active');
 }); 
