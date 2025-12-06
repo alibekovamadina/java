e4f251a860c52387a272e84bb168a6b473b67dcf
itschool <itschool@tou.edu.kz>
2025-12-06 11:49:45 +0500

    prepare

--- script.js
+++ script.js
@@ -33,24 +33,120 @@ document.addEventListener('DOMContentLoaded', function () {
     const popupFilter = document.querySelector('.popup-filter');
 
     if (popupFilter) {
-        const buttonPopupClose = document.querySelector('.popup-filter__close');
-        buttonPopup.addEventListener('click', function () {
-            const popupFilter = document.querySelector('.popup-filter');
-            if (popupFilter) {
+        const buttonPopup = document.querySelector('.catalog__filter');
+        if (buttonPopup) {
+            buttonPopup.addEventListener('click', function () {
                 document.body.classList.add('no-scroll');
                 popupFilter.classList.add('popup-filter--show')
-                setTimeout(function() {
+                setTimeout(function () {
                     popupFilter.classList.add('popup-filter--active')
                 }, 10);
+            })
+        }
+
+        const buttonPopupClose = document.querySelector('.popup-filter__close');
+        if (buttonPopupClose) {
+            buttonPopupClose.addEventListener('click', function () {
+                popupFilter.classList.remove('popup-filter--active')
+                setTimeout(function () {
+                    popupFilter.classList.remove('popup-filter--show')
+                    document.body.classList.remove('no-scroll');
+                }, 300);
+            });
+        }
+    }
+
+    let titles = [
+        "Общее",
+        "Сыр",
+        "Мясо",
+        "Компонент"
+    ];
+
+    let buttons = [
+        [
+            'Хит', 
+            'Новинка', 
+            'С мясом', 
+            'Вегетерианская', 
+            'С курицей', 
+            'Без лука', 
+            'С грибами', 
+            'С морепродуктами', 
+            'Барбекю'
+        ],
+        [
+            'Реджанито',
+            'Моцарелла',
+            'Чеддер',
+            'С голубой плесенью',
+            'Смесь итальянских сыров',
+            'Мягкий молодой сыр'
+        ],
+        [
+            'Пепперони',
+            'Свинина',
+            'Ветчина',
+            'Бекон',
+            'Говядина',
+            'Чоризо',
+            'Колбаски',
+            'Куриная грудка'
+        ],
+        [
+            'Креветка',
+            'Ананасы',
+            'Шампиньоны',
+            'Лук',
+            'Перец халапеньо',
+            'Орегано',
+            'Зеленый перец',
+            'Томаты',
+            'Чеснок',
+            'Красный перец',
+            'Оливки',
+            'Маслины',
+            'Клубника',
+            'Смесь итальянских трав'
+        ]
+    ];
+
+    const groups = document.querySelector('.popup-filter__groups');
+    if (groups) {
+        let groupHtml = '';
+        for(let i = 0; i < titles.length; i = i + 1) { 
+            let buttonsHtml = '';
+            for(let j = 0; j < buttons[i].length; j = j + 1) {
+                buttonsHtml = buttonsHtml + `<button>${buttons[i][j]}</button>`
             }
-        })
+            groupHtml = groupHtml + `<div class="popup-filter__group">
+                        <h4>${titles[i]}</h4>
+                        <div class="popup-filter__checkbox">
+                            ${buttonsHtml}
+                        </div> 
+                    </div>`;
+        }
+        groups.innerHTML = groupHtml;
     }
+});
+        //list.textContent = "hello world"//
+        //list.innerHTML = "<h2>hello<br>world</h2>"//
+        //list.outerHTML = "<div class='popup-filter__checkbox active'><button>hello</button></div>"//
+        
+    //    for(let i = 0; i < buttons.length; i = i + 1) {
+    //        let newElement = document.createElement('button')
+    //        newElement.textContent = buttons [i];
+    //       list.append(newElement)
+    //    }
+    
 
-    const buttonPopupClose = document.querySelector('.popup-filter__close');
-    if (buttonPopupClose) {
-        buttonPopupClose.addEventListener('click', function () {
 
-        })
-    }
 
-}) 
+//let m = [5, 4, 5, 2];
+//console.log(m[0])
+//m[1] = 7
+//console.log(m);
+//m[4] = 9
+//console.log(m[10])
+//m.push(588) 
+//m.length
